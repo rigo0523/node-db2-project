b36a454d34c27424a0b2bf87cd4a85cbe55cfb2a
Rigo <limon1285@gmail.com>
2020-11-16 23:13:04 -0600

    MVP Done

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ server.post("/cars", async (req, res) => {
 
 const port = 8000;
 server.listen(port, () => {
-  console.log(`******Server is running on ${port}******`);
+  console.log(`******Server is running on ${port}*****`);
 });
